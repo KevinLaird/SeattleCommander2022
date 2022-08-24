afa6d003256c239bcbe075d4fff95d0682d14beb
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-08-24 15:54:05 -0700

    Modularized Navbar. Removed list items for now

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import './Navbar.scss';
+import styles from './Navbar.module.scss';
 import NavBrand from './NavBrand';
 import NavItem from './NavItem';
 
-const NAVBAR_ITEMS = [
-  {
-    id: '01',
-    name: 'Events',
-  },
-  {
-    id: '02',
-    name: 'About',
-  },
+const NAVBAR_ITEMS: any[] = [
+  // {
+  //   id: '01',
+  //   name: 'About',
+  //   link: '#',
+  // },
 ];
 
 const Navbar = (): JSX.Element => (
-  <nav className="Navbar-container">
+  <nav className={styles.container}>
     <NavBrand />
-    <ul className="Navbar-items">
-      {NAVBAR_ITEMS.map((item) => <NavItem key={item.id} name={item.name} />)}
+    <ul className={styles.items}>
+      {NAVBAR_ITEMS.length !== 0 && NAVBAR_ITEMS.map((item) => (
+        <NavItem
+          key={item.id}
+          name={item.name}
+          link={item.link}
+        />
+      ))}
     </ul>
   </nav>
 );
