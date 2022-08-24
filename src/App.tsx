979606bfe455aae2c34e7bc8c9724bce2024dab4
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-08-24 15:56:25 -0700

    Modularized app. Restructured content

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './css/main.scss';
+import styles from './App.module.scss';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import Watermark from './components/Watermark/Watermark';
 
 const App = (): JSX.Element => {
   const discordLink = 'https://discord.com/invite/EAMvygbY9c';
   return (
-    <div className="App-container">
+    <div className={styles.container}>
       <Navbar />
-      <Watermark />
-      <div className="App-container--content">
-        <h1 className="title">Seattle Commander</h1>
-        <h2>An inclusive group that enjoys playing Magic: The Gathering</h2>
-        <h3>
-          <a href={discordLink} className="btn btn--1">Join Now!</a>
-        </h3>
+      <div className={styles.flexContainer}>
+        <div className={styles.contentContainer}>
+          <h1 className={styles.title}>Seattle Commander</h1>
+          <h2 className={styles.content}>
+            A place where people can come together and play Magic in the
+            Seattle Area.
+          </h2>
+          <a
+            href={discordLink}
+            className={`btn btn--2 ${styles.btn}`}
+          >
+            Join Now!
+          </a>
+        </div>
+
       </div>
       <Footer />
     </div>
